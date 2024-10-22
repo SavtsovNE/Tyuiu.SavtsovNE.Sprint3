69775e07f11ff5e4e8d044ba3fcc30ac02ea65b8
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-22 23:40:57 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.SavtsovNE.Sprint3.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SavtsovNE.Sprint3.Task0.V23.Lib;
+
+namespace Tyuiu.SavtsovNE.Sprint3.Task0.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            double value = 1.5;
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 0.298;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
