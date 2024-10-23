d53fb370bb490c5061ee09787b2c6d8992521dea
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-24 03:04:11 +0500

    Task1

--- Tyuiu.SavtsovNE.Sprint3.Task1.V21.Lib/DataService.cs
+++ Tyuiu.SavtsovNE.Sprint3.Task1.V21.Lib/DataService.cs
@@ -14,10 +14,16 @@ namespace Tyuiu.SavtsovNE.Sprint3.Task1.V21.Lib
             double multSeries = 1;
             while (startValue <= stopValue)
             {
-                multSeries *= Math.Pow((0.5 + (startValue / 5)) / (Math.Cos(value) + 0.5), 3);
-                startValue++ ;
+                double currentVal = (0.5 + (startValue / 5)) / (Math.Cos(value) + 0.5);
+                double powVal = Math.Pow(currentVal, 3);
+                multSeries *= powVal;
+
+                Console.WriteLine($"StartValue: {startValue}, CurrentVal: {currentVal}, PowVal: {powVal}, MultSeries: {multSeries}");
+
+                startValue++;
             }
             return Math.Round(multSeries, 3);
         }
+
     }
 }
