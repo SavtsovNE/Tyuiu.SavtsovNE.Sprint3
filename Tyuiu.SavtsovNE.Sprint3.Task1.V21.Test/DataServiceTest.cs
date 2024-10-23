12a25281e149c6b6c059f5344ba4b011d52901c0
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-24 02:53:40 +0500

    Task1

--- Tyuiu.SavtsovNE.Sprint3.Task1.V21.Test/DataServiceTest.cs
+++ Tyuiu.SavtsovNE.Sprint3.Task1.V21.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.SavtsovNE.Sprint3.Task1.V21.Test
             int value = 1;
             int startValue = 1;
             int stopValue = 12;
-            double res = 16947810.881;
+            double res = 99.512;
             Assert.AreEqual(res, ds.GetMultiplySeries(value, startValue, stopValue));
         }
     }
