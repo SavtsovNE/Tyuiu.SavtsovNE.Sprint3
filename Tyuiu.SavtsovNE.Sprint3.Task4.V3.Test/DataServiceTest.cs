7a9a2baaae7b134ca0c3ddd164b456c2868695a8
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-25 16:25:42 +0500

    Task4

---
+++ Tyuiu.SavtsovNE.Sprint3.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.SavtsovNE.Sprint3.Task4.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+        }
+    }
+}
---
+++ Tyuiu.SavtsovNE.Sprint3.Task4.V3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SavtsovNE.Sprint3.Task4.V3.Lib;
+
+namespace Tyuiu.SavtsovNE.Sprint3.Task4.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 │ Выполнил: Савцов Никита Евгеньевич | ИБКСб - 24 - 1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Вынолнил: Савцов Никита Евгеньевич | ИБКСб - 24 - 1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить значения суммы функции на отрезке от -5 до 5                  *");
+            Console.WriteLine("* и оборвать вычисления при x = 0                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Сумма функций - " + ds.Calculate(-5, 5));
+            Console.ReadKey();
+        }
+    }
+}
