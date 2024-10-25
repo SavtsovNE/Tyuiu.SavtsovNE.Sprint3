364763964f33917b6e3c3f209198935bf9673e71
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-25 17:45:46 +0500

    Task5

---
+++ Tyuiu.SavtsovNE.Sprint3.Task5.V16/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SavtsovNE.Sprint3.Task5.V16.Lib;
+
+namespace Tyuiu.SavtsovNE.Sprint3.Task5.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Савцов Никита Евгеньевич | ИБКСб - 24 - 1 ";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Савцов Никита Евгеньевич | ИБКСб - 24 - 1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* y = sum i = 1 to 3 sum k = 1 to 10 cos(k) + x ^ 2 при x = 2.            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 2;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+
+            Console.WriteLine("Переменная X = " + x);
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+            Console.ReadKey();
+        }
+    }
+}
